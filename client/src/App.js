e6d738f886a4a216d915fdbf9def28d0519fca94
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-01-12 03:48:19 +0000

    implementing pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import Home from './components/screens/Home'
+import Login from './components/screens/Login'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
+
 
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+        <NavBar />
+        <Route path='/'><Home/></Route>
+        <Route path='/login'><Login/></Route>
+        <Route path='/signup'><Signup/></Route>
+        <Route path='/profile'><Profile/></Route>
+
+
+
+
+    </BrowserRouter>
+
 
   );
 }
