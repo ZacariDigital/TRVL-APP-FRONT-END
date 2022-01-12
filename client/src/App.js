ea04891e9e7c0b692d5f730df051cdc755d529bb
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-01-12 04:30:13 +0000

    pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar'
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom'
-
+import {BrowserRouter,Route} from 'react-router-dom'
 import Home from './components/screens/Home'
 import Login from './components/screens/Login'
 import Profile from './components/screens/Profile'
@@ -13,11 +12,16 @@ import Signup from './components/screens/Signup'
 function App() {
   return (
     <BrowserRouter>
-        <NavBar />
-        <Route path='/'><Home/></Route>
-        <Route path='/login'><Login/></Route>
-        <Route path='/signup'><Signup/></Route>
-        <Route path='/profile'><Profile/></Route>
+        <NavBar>
+          <Route path="/"><Home/></Route>
+          <Route path="/login"><Login/></Route>
+          <Route path="/signup"><Signup/></Route>
+          <Route path="/profile"><Profile/></Route>
+
+
+
+        </NavBar>
+
 
 
 
