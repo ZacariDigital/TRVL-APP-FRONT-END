e84b4cf5cb89970c6137c627c1453db8d0a5e1f0
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-01-26 12:43:54 +0000

    require login before posting any adverts

--- client/src/components/Createpost.js
+++ client/src/components/Createpost.js
@@ -1,10 +1,50 @@
 import React,{useState} from 'react'
 
 const CreatePost = ()=>{
-    const[title, setTitle] = useState("")
-    const [body, setBody] = useState("")
-    const [price, setPrice] = useState("")
-    const [image, setImage] = useState("")
+    const [title,setTitle] = useState("")
+    const [body,setBody] = useState()
+    const [price,setPrice] = useState("")
+    const [image,setImage] = useState("")
+    const [url,setUrl] = useState("")
+
+
+    const postDetails = () =>{
+        const data = new FormData()
+        data.append("file", image)
+        data.append("upload_preset", "trvlappimages")
+        data.append("cloudname", "dayv3dwwn")
+        fetch("https://api.cloudinary.com/v1_1/dayv3dwwn/image/upload",{
+            method:"post",
+            body:data
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            setUrl(data.url)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+        fetch("./createpost",{
+            method:"post",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+
+                title,
+                body,
+                price,
+                pic:url
+            })
+
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    
 
     return(
         <div className='card input-filed'
@@ -30,12 +70,14 @@ const CreatePost = ()=>{
             <div className="file-field input-field">
                 <div class="btn">
                 <span>Upload Image</span>
-                <input type="file" onChange={(e)=>console.setImage(e.target.files[0])}/>
+                <input type="file" onChange={(e)=>setImage(e.target.files[0])}/>
                 </div>
                 <div class="file-path-wrapper">
                 <input class="file-path validate" type="text"/>
             </div>
-            <button className="btn waves-effect waves-darken">
+            <button className="btn waves-effect waves-darken"
+            onClick={()=>postDetails()}
+            >
                     Submit
                 </button>
             </div>
